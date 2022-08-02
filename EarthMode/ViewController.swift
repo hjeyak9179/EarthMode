719651858bc58d418f73f85c82ef34c16f858445
Macarena Alvarez <alvarezmacuchi@gmail.com>
2022-08-02 18:30:36 -0300

    Maca's intro

--- EarthMode/ViewController.swift
+++ EarthMode/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var earthmodeImageView: UIImageView!
+    @IBOutlet weak var missionTextField: UITextField!
+    @IBOutlet weak var missionTextView: UITextView!
+    @IBOutlet weak var aboutusButton: UIButton!
+    @IBOutlet weak var optionsTextField: UITextField!
+    @IBOutlet weak var outfitButton: UIButton!
+    @IBOutlet weak var quizButton: UIButton!
+    @IBOutlet weak var ecologButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
