cfebd08e237410c4bb73fa8f0a2c7f0b271428e4
Laura Wright <lwrighthfx@gmail.com>
2022-08-03 12:58:51 -0300

    Laura: 11:58pm EST

---
+++ EarthMode/LearnViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LearnViewController.swift
+//  EarthMode
+//
+//  Created by Laura Wright on 2022-08-03.
+//
+
+import UIKit
+
+class LearnViewController: UIViewController {
+
+    @IBOutlet weak var outfitLabel: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
