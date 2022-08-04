250dcc0fb0acb10f721258924393420b9321fd86
Harshitha Jeyakumar <harshyjey@gmail.com>
2022-08-03 21:43:56 -0400

    HARSHITHA: EST 9:43, FINISHED THE ECO LOG FEATURE FULLY

--- EarthMode/EcoLogViewController.swift
+++ EarthMode/EcoLogViewController.swift
@@ -9,13 +9,56 @@ import UIKit
 
 class EcoLogViewController: UIViewController {
 
+    @IBOutlet weak var walkInput: UITextField!
+    @IBOutlet weak var waterInput: UITextField!
+    @IBOutlet weak var recycleSwitch: UISwitch!
+    @IBOutlet weak var reflectInput: UITextView!
+    @IBOutlet weak var eatSwitch: UISwitch!
+    @IBOutlet weak var reusableSwitch: UISwitch!
+    @IBOutlet weak var improveOutput: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    func learnMore () -> String {
+        var recycled = ""
+        var walkNum = ""
+        let waterNum = " Conserve Water "
+        var diet = ""
+        var reusable = ""
+        if recycleSwitch.isOn == false {
+            recycled = " Start Recycling "
+        }
+        if eatSwitch.isOn == false {
+            diet = " Eat More Plant-Based "
+        }
+        if reusableSwitch.isOn == false {
+            reusable = " Don't Use Single-Use Products "
+        }
+        if walkInput.text == "0" {
+            walkNum = " Walk More "
+        }
+        return walkNum + waterNum + recycled + diet + reusable
+    }
+    
+    
+    @IBAction func resetButton(_ sender: Any) {
+        walkInput.text = ""
+        waterInput.text = ""
+        recycleSwitch.setOn(false, animated: true)
+        eatSwitch.setOn(false, animated: true)
+        reusableSwitch.setOn(false, animated: true)
+        reflectInput.text = ""
+    }
+    
+    
+    @IBAction func doneButton(_ sender: Any) {
+        improveOutput.text = learnMore()
+    }
+    
     /*
     // MARK: - Navigation
 
