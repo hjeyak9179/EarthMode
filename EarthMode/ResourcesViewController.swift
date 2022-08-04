8f48ffa1acb3434ffcaea8d43c8bdb297b88cba2
Laura Wright <lwrighthfx@gmail.com>
2022-08-04 17:05:33 -0300

    LAURA, ADDED RESOURCES CHECKED TO MAKE SURE IT WAS WORKING!! 4:05EST

--- EarthMode/ResourcesViewController.swift
+++ EarthMode/ResourcesViewController.swift
@@ -9,13 +9,35 @@ import UIKit
 
 class ResourcesViewController: UIViewController {
 
+    @IBOutlet weak var everlaneButton: UIButton!
+    @IBOutlet weak var redoneButton: UIButton!
+    @IBOutlet weak var thredupButton: UIButton!
+    @IBOutlet weak var vogueButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func everlaneButton(_ sender: Any) {
+        let url = URL (string:"https://www.everlane.com/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func redoneButton(_ sender: Any) {
+        let url = URL (string: "https://shopredone.com/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func thredupButton(_ sender: Any) {
+        let url = URL (string:"https://www.thredup.com/fashionfootprint/")!
+        UIApplication.shared.open (url)
+    }
+    
+    @IBAction func vogueButton(_ sender: Any) {
+        let url = URL (string: "https://www.vogue.co.uk/fashion/article/sustainable-fashion")!
+        UIApplication.shared.open (url)
+    }
     /*
     // MARK: - Navigation
 
