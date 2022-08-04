6f39bcb485c1f54cd07a333c03c08b4bf7295802
Macarena Alvarez <alvarezmacuchi@gmail.com>
2022-08-04 03:35:31 -0300

    Macarena: tried constraints and added information in the outfit generator

--- EarthMode/LearnViewController.swift
+++ EarthMode/LearnViewController.swift
@@ -16,7 +16,7 @@ class LearnViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func learnButton(_ sender: Any) {
-        outfitLabel.text = "hey hey"
+        outfitLabel.text = "A polyester shirt produces the equivalent of 5.5kg of carbon dioxide compared to 2.1kg from a cotton shirt. Jeans manufacturer, Levi Strauss, estimates that a pair of its iconic 501 jeans will produce the equivalent of 33.4kg of carbon dioxide equivalent across its entire lifespan – about the same as driving 69 miles in the average US car. Cow skin leather boots produce 66.0kg of CO2e and a cow skin leather bag produces 100.5kg of CO2e. A denim bucket-hat produces around 5kg of CO2e."
     }
     
 
